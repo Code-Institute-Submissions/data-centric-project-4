49fc50f4ecbe25d15ae279e192832effc716fd69
Helmy <chanhelmy@hotmail.com>
2019-05-03 07:21:54 +0000

    Add difficulty app routes to app py

--- app.py
+++ app.py
@@ -17,7 +17,12 @@ def getindex():
     # for d in found_cuisine:
     #     print(d)
     # found_cuisine.rewind()
-    return render_template("index.html",collection_recipes=found,collection_cuisine=found_cuisine)
+    collection_skill = conn['cookingRecipes']['skill_level']
+    found_skill = list(collection_skill.find({}))
+    # for a in found_skill:
+    #     print(a)
+    # found_skill.rewind()
+    return render_template("index.html",collection_recipes=found,collection_cuisine=found_cuisine,collection_skill = found_skill)
 
 @app.route('/msia_cuisine')
 def msia_cuisine():
@@ -43,14 +48,41 @@ def indian_cuisine():
     #     print(d)
     return render_template("index.html",collection_recipes = found)
 
+@app.route('/easy_recipes')
+def easy_recipes():
+    collection_recipes = conn['cookingRecipes']['recipes']
+    found = list(collection_recipes.find({'difficulty': 'Easy'}))
+    # for d in found:
+    #     print(d)
+    return render_template("index.html",collection_recipes = found)
+
+@app.route('/medium_recipes')
+def medium_recipes():
+    collection_recipes = conn['cookingRecipes']['recipes']
+    found = list(collection_recipes.find({'difficulty': 'Medium'}))
+    # for d in found:
+    #     print(d)
+    return render_template("index.html",collection_recipes = found)
+
+@app.route('/hard_recipes')
+def hard_recipes():
+    collection_recipes = conn['cookingRecipes']['recipes']
+    found = list(collection_recipes.find({'difficulty': 'Hard'}))
+    # for d in found:
+    #     print(d)
+    return render_template("index.html",collection_recipes = found)
+
 @app.route('/add', methods =['GET','POST'])
 def addrecipe():
     if request.method =='GET':
         collection_cuisine= conn['cookingRecipes']['cuisine']
         found_cuisine =  collection_cuisine.find({})
-        return render_template("add.html",collection_cuisine=found_cuisine)
+        collection_skill = conn['cookingRecipes']['skill_level']
+        found_skill = collection_skill.find({})
+        return render_template("add.html",collection_cuisine=found_cuisine,collection_skill=found_skill)
     else:
         name_recipe = request.form['recipename']
+        skill_level = request.form['difficulty']
         num_calories = request.form['numofcalories']
         serving_size = request.form['servingsize']
         recipe_author = request.form['recipeauthor']
@@ -62,6 +94,7 @@ def addrecipe():
         collection_recipes = conn['cookingRecipes']['recipes']
         collection_recipes.insert({
             'name': name_recipe,
+            'difficulty' : skill_level,
             'num_of_calories_per_serving': num_calories,
             'num_of_serving': serving_size,
             'recipe_author' : recipe_author,
@@ -83,9 +116,12 @@ def edit_recipe(recipe_id):
         })
         collection_cuisine= conn['cookingRecipes']['cuisine']
         found = collection_cuisine.find({})
-        return render_template('edit.html',getrecipe=collection_from_db, collection_cuisine=list(found))
+        collection_skill = conn['cookingRecipes']['skill_level']
+        found_skill = collection_skill.find({})
+        return render_template('edit.html',getrecipe=collection_from_db, collection_cuisine=list(found), collection_skill=list(found_skill))
     else:
         name_recipe = request.form['recipename']
+        skill_level = request.form['difficulty']
         num_calories = request.form['numofcalories']
         serving_size = request.form['servingsize']
         recipe_author = request.form['recipeauthor']
@@ -99,6 +135,7 @@ def edit_recipe(recipe_id):
         },{
             '$set':{
             'name': name_recipe,
+            'difficulty' : skill_level,
             'num_of_calories_per_serving': num_calories,
             'num_of_serving': serving_size,
             'recipe_author' : recipe_author,
